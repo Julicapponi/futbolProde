257c56fb2743de15334b1e5e446f71f6b3c5da18
Jcapponi <julicapponi@hotmail.com>
2023-02-21 03:03:12 -0300

    filtros fechas

--- src/app/list-enfrentamientos/list-enfrentamientos.page.ts
+++ src/app/list-enfrentamientos/list-enfrentamientos.page.ts
@@ -63,7 +63,6 @@ export class ListEnfrentamientosPage implements OnInit, OnDestroy {
     golLocal: 0,
     golVisitante: 0,
   };
-
   index: number;
   partidosFiltrados: Enfrentamiento[];
 
@@ -72,6 +71,9 @@ export class ListEnfrentamientosPage implements OnInit, OnDestroy {
   realizoCalculo = false;
   todosLosPartidos: Enfrentamiento[];
   restarsumar_ocupado=false;
+  fechaAVisualizarPorActualidad: any;
+  partidosAVisualizar: Enfrentamiento[];
+   
 
   constructor(private toast: ToastController, private router: Router, private sharingService: SharingServiceService, private route: ActivatedRoute, private menuCtrl: MenuController,
               private authService: AuthService, private resultService: ResultsService,
@@ -79,18 +81,22 @@ export class ListEnfrentamientosPage implements OnInit, OnDestroy {
               public datepipe: DatePipe, private comparteDatosService: ComparteDatosService, private cdr: ChangeDetectorRef, private pronosticosService: PronosticosService) {
     this.obtenerPartidosPronosticados();
     this.obtenerPartidos();
-    this.obtenerFiltroFechas(this.partidos);
+    //this.obtenerFiltroFechas(this.partidos);
+    this.obtenerFechaDefault(this.partidos);
+    this.filtrarPartidosPorFecha(this.fechaAVisualizarPorActualidad);
+
   }
 
   async obtenerPartidos() {
     this.isCargandoPartidos = true;
     await this.sharingService.obtenerPartidos.subscribe((data: Enfrentamiento[]) => {
-      //this.partidos = data;
-      this.partidos = data.slice(0, 20);
+      this.partidos = data;
+      //this.partidos = data.slice(0, 80);
       this.todosLosPartidos = this.partidos;
       this.isCargandoPartidos = false;
     });
   }
+  
 
   async obtenerPartidosPronosticados() {
     let idCompetencia = localStorage.getItem('idCompetenciaSeleccionada');
@@ -112,7 +118,6 @@ export class ListEnfrentamientosPage implements OnInit, OnDestroy {
           }
           //CONCATENEO LOS PARTIDOS PRONOSTICADOS CON LOS PARTIDOS TRAIDOS DE LA API
           this.partidos = data.concat(this.partidos);
-          console.log(this.partidos);
         });
   }
 
@@ -121,17 +126,146 @@ export class ListEnfrentamientosPage implements OnInit, OnDestroy {
       return partido.round;
     });
     var sorted = rounds.sort();
-
+    
     this.fechasCompetencia = sorted.filter(function (value, index) {
       return value !== sorted[index + 1];
     });
   }
 
+  compareStrings(a, b) {
+    const regex = /(\d+)/g;
+    let aMatches: any;
+    let bMatches: any;
+    if(a===null || b===null){
+      // Si uno de los strings no contiene números, tratarlo como 0
+      aMatches = [0];
+      bMatches = [0];
+    } else{
+
+      aMatches = a.match(regex);
+      bMatches = b.match(regex);
+    }
+    
+    // Convertir strings a números y llenar con ceros a la izquierda para que tengan la misma longitud
+    aMatches = aMatches.map((match) => parseInt(match)).map((num) => num.toString().padStart(2, "0"));
+    bMatches = bMatches.map((match) => parseInt(match)).map((num) => num.toString().padStart(2, "0"));
+
+    // Comparar los números
+    for (let i = 0; i < Math.min(aMatches.length, bMatches.length); i++) {
+      if (aMatches[i] !== bMatches[i]) {
+        return aMatches[i] - bMatches[i];
+      }
+    }
+
+    // Si los números son iguales, comparar los strings completos
+    return a.localeCompare(b);
+  }
+  
+  obtenerFechaDefault(partidos) {
+    let splitHoy = [];
+    let mesActual:string;
+    let diaActual:string;
+    let dateHoy = new Date();
+    
+    //año
+    const anioActual = dateHoy.getFullYear();
+    
+    //mes
+    mesActual = (dateHoy.getMonth() + 1).toString();
+    if(mesActual.toString().length == 1){
+      mesActual = String(mesActual).padStart(2,'0').toString();
+    }
+    
+    //dia
+    diaActual = dateHoy.getDate().toString();
+    if(diaActual.toString().length == 1){
+      diaActual = String(diaActual).padStart(2,'0').toString();
+    }
+    const fechaActual = anioActual+'-'+mesActual+'-'+diaActual+'T00:00:00+00:00';
+    var json = partidos.map(function (partido) {
+      return {round: partido.round, fechaEnfrentamiento: partido.fechaEnfrentamiento};
+    });
+
+    const result = {};
+
+    for (const obj of json) {
+      if (result[obj.round] === undefined) {
+        result[obj.round] = {
+          round: obj.round,
+          minFechaEnfrentamiento: obj.fechaEnfrentamiento,
+          maxFechaEnfrentamiento: obj.fechaEnfrentamiento,
+        };
+      } else {
+        if (obj.fechaEnfrentamiento < result[obj.round].minFechaEnfrentamiento) {
+          result[obj.round].minFechaEnfrentamiento = obj.fechaEnfrentamiento;
+        }
+        if (obj.fechaEnfrentamiento > result[obj.round].maxFechaEnfrentamiento) {
+          result[obj.round].maxFechaEnfrentamiento = obj.fechaEnfrentamiento;
+        }
+      }
+    }
+    
+    //OBTIENE LAS DISTINTAS FECHAS PARA EL SELECTOR
+    
+    for (const res in result) {
+      this.fechasCompetencia.push(res);
+    }
+    this.fechasCompetencia.sort(this.compareStrings);
+    
+    
+    let resultArray = [];
+    resultArray = Object.values(result);
+    //ordeno por round
+    resultArray = resultArray.sort((a, b) => {
+      const aParts = a.round.split(" ");
+      const bParts = b.round.split(" ");
+
+      const aPhaseNum = parseInt(aParts[aParts.length - 1]);
+      const bPhaseNum = parseInt(bParts[bParts.length - 1]);
+
+      if (aParts[0] !== bParts[0]) {
+        return aParts[0].localeCompare(bParts[0]);
+      }
+
+      return aPhaseNum - bPhaseNum;
+    });
+    
+    const currentDate = new Date();
+    
+  // Filtra los elementos del arreglo que contienen la fecha actual entre su minimo y maximo, es decir, entre el 1er partido y el ultimo de la fecha
+    const currentRounds = resultArray.filter(item => {
+      if(item.round.includes('1st Phase - 4')){
+        console.log('');
+      }
+      const minDate = new Date(item.minFechaEnfrentamiento);
+      const maxDate = new Date(item.maxFechaEnfrentamiento);
+      return minDate <= currentDate && currentDate <= maxDate;
+    });
+
+    // Si hay algún round/fecha actual, devuelve el primer elemento
+    if (currentRounds.length > 0) {
+      const currentRound = currentRounds[0];
+      console.log(currentRound.round);
+      this.fechaAVisualizarPorActualidad = currentRound.round;
+    } else {
+      // Si no hay ningún round actual, encuentra el round más cercano a la fecha actual
+      const upcomingRound = resultArray.find(item => {
+        const minDate = new Date(item.minFechaEnfrentamiento);
+        return minDate > currentDate;
+      });
+      this.fechaAVisualizarPorActualidad = upcomingRound.round;
+      console.log(upcomingRound.round);
+    }
+    return this.fechaAVisualizarPorActualidad
+  }
+
 
   filtrarPartidosPorFecha(fecha: string) {
     console.log('Seleccionaste esta fecha:', fecha);
-    this.fecha = fecha;
-    var prueba = this.todosLosPartidos.filter(partido => partido.round.includes(this.fecha));
+    this.fecha = fecha.trim();
+    this.partidos = [];
+    this.partidos = this.todosLosPartidos.filter(partido => partido.round ===this.fecha);
+    this.partidosAVisualizar = this.partidos
   }
 
   ngOnDestroy(): void {
@@ -460,10 +594,6 @@ export class ListEnfrentamientosPage implements OnInit, OnDestroy {
   async fechaEvent($event: any) {
     console.log($event);
     const fecha = $event.toString();
-    if(fecha.includes('2nd Round')){
-      this.fechaCompara = fecha;
-      this.fechaComp = 'Fecha 2';
-    }
     this.filtrarPartidosPorFecha(fecha);
   }
 
--- src/app/modificar-enfrentamientos/modificar-enfrentamientos.page.ts
+++ src/app/modificar-enfrentamientos/modificar-enfrentamientos.page.ts
@@ -30,6 +30,12 @@ export class ModificarEnfrentamientosPage implements OnInit {
     private data$: Observable<Comp>;
     compActivas: string;
     competenciaSeleccionada: any;
+    seleccionoUnaComp=false;
+    fecha: string;
+    todosLosPartidos: any;
+    partidosAVisualizar: Enfrentamiento[];
+    fechaAVisualizarPorActualidad: any;
+    prueba: Enfrentamiento[];
   
   constructor(private toast: ToastController, private activatedRoute: ActivatedRoute, private competenciaService: CompetenciaService, private sharingService: SharingServiceService, private router: Router, private menuCtrl: MenuController, private authService: AuthService, public alertController: AlertController, private competitionsService: CompetenciaService) {
       this.competenciasActiv();
@@ -42,28 +48,166 @@ export class ModificarEnfrentamientosPage implements OnInit {
 
     listEnfrentamientosBD(idCompetenciaSeleccionada: string, anioCompetenciaSeleccionada: string){
       this.isCargando = true;
-    this.competenciaService.getEnfrentamientos(idCompetenciaSeleccionada, anioCompetenciaSeleccionada).subscribe(res => {
+    this.competenciaService.getEnfrentamientos(idCompetenciaSeleccionada, anioCompetenciaSeleccionada).subscribe(async res => {
         console.log(res);
         //timeout por los logos de equipos, tardan en cargar/impactar en la pagina
-        setTimeout(async () => {
-            this.isCargando = false;
-        }, 1500);
-        this.enfrentamientos = res.slice(0, 20);
-        if(this.enfrentamientos.length == 0){
+        this.isCargando = false;
+        this.enfrentamientos = res;
+        //this.enfrentamientos = res.slice(0, 20);
+        await this.obtenerFechaDefault(this.enfrentamientos);
+        await this.filtrarPartidosPorFecha(this.fechaAVisualizarPorActualidad);
+        if (this.enfrentamientos.length == 0) {
             setTimeout(async () => {
-                if(this.enfrentamientos.length == 0){
+                if (this.enfrentamientos.length == 0) {
                     this.showToastMessage('Puede que no se visualicen enfrentamientos ya que la competencia fue activa el día de hoy, espere a las 00:00 hs. cuando se sincronicen los enfrentamientos', 'danger', 'thumbs-down');
                 }
             }, 2000);
         }
-       
-        
+
+
         err => {
             console.log(err);
         }
     });
   }
 
+
+    filtrarPartidosPorFecha(fecha: string) {
+        console.log('Seleccionaste esta fecha:', fecha);
+        this.fecha = fecha.trim();
+        this.partidos = [];
+        this.partidosAVisualizar = [];
+        this.partidosAVisualizar = this.enfrentamientos.filter(partido => partido.round ===this.fecha);
+    }
+
+    compareStrings(a, b) {
+        const regex = /(\d+)/g;
+        let aMatches: any;
+        let bMatches: any;
+        if(a===null || b===null){
+            // Si uno de los strings no contiene números, tratarlo como 0
+            aMatches = [0];
+            bMatches = [0];
+        } else{
+
+            aMatches = a.match(regex);
+            bMatches = b.match(regex);
+        }
+
+        // Convertir strings a números y llenar con ceros a la izquierda para que tengan la misma longitud
+        aMatches = aMatches.map((match) => parseInt(match)).map((num) => num.toString().padStart(2, "0"));
+        bMatches = bMatches.map((match) => parseInt(match)).map((num) => num.toString().padStart(2, "0"));
+
+        // Comparar los números
+        for (let i = 0; i < Math.min(aMatches.length, bMatches.length); i++) {
+            if (aMatches[i] !== bMatches[i]) {
+                return aMatches[i] - bMatches[i];
+            }
+        }
+
+        // Si los números son iguales, comparar los strings completos
+        return a.localeCompare(b);
+    }
+    
+    
+    obtenerFechaDefault(partidos) {
+        let splitHoy = [];
+        let mesActual:string;
+        let diaActual:string;
+        let dateHoy = new Date();
+
+        //año
+        const anioActual = dateHoy.getFullYear();
+
+        //mes
+        mesActual = (dateHoy.getMonth() + 1).toString();
+        if(mesActual.toString().length == 1){
+            mesActual = String(mesActual).padStart(2,'0').toString();
+        }
+
+        //dia
+        diaActual = dateHoy.getDate().toString();
+        if(diaActual.toString().length == 1){
+            diaActual = String(diaActual).padStart(2,'0').toString();
+        }
+        const fechaActual = anioActual+'-'+mesActual+'-'+diaActual+'T00:00:00+00:00';
+        var json = partidos.map(function (partido) {
+            return {round: partido.round, fechaEnfrentamiento: partido.fechaEnfrentamiento};
+        });
+
+        const result = {};
+
+        for (const obj of json) {
+            if (result[obj.round] === undefined) {
+                result[obj.round] = {
+                    round: obj.round,
+                    minFechaEnfrentamiento: obj.fechaEnfrentamiento,
+                    maxFechaEnfrentamiento: obj.fechaEnfrentamiento,
+                };
+            } else {
+                if (obj.fechaEnfrentamiento < result[obj.round].minFechaEnfrentamiento) {
+                    result[obj.round].minFechaEnfrentamiento = obj.fechaEnfrentamiento;
+                }
+                if (obj.fechaEnfrentamiento > result[obj.round].maxFechaEnfrentamiento) {
+                    result[obj.round].maxFechaEnfrentamiento = obj.fechaEnfrentamiento;
+                }
+            }
+        }
+
+        //OBTIENE LAS DISTINTAS FECHAS PARA EL SELECTOR
+
+        for (const res in result) {
+            this.fechasCompetencia.push(res);
+        }
+        this.fechasCompetencia.sort(this.compareStrings);
+
+
+        let resultArray = [];
+        resultArray = Object.values(result);
+        //ordeno por round
+        resultArray = resultArray.sort((a, b) => {
+            const aParts = a.round.split(" ");
+            const bParts = b.round.split(" ");
+
+            const aPhaseNum = parseInt(aParts[aParts.length - 1]);
+            const bPhaseNum = parseInt(bParts[bParts.length - 1]);
+
+            if (aParts[0] !== bParts[0]) {
+                return aParts[0].localeCompare(bParts[0]);
+            }
+
+            return aPhaseNum - bPhaseNum;
+        });
+
+        const currentDate = new Date();
+
+        // Filtra los elementos del arreglo que contienen la fecha actual entre su minimo y maximo, es decir, entre el 1er partido y el ultimo de la fecha
+        const currentRounds = resultArray.filter(item => {
+            if(item.round.includes('1st Phase - 4')){
+                console.log('');
+            }
+            const minDate = new Date(item.minFechaEnfrentamiento);
+            const maxDate = new Date(item.maxFechaEnfrentamiento);
+            return minDate <= currentDate && currentDate <= maxDate;
+        });
+
+        // Si hay algún round/fecha actual, devuelve el primer elemento
+        if (currentRounds.length > 0) {
+            const currentRound = currentRounds[0];
+            console.log(currentRound.round);
+            this.fechaAVisualizarPorActualidad = currentRound.round;
+        } else {
+            // Si no hay ningún round actual, encuentra el round más cercano a la fecha actual
+            const upcomingRound = resultArray.find(item => {
+                const minDate = new Date(item.minFechaEnfrentamiento);
+                return minDate > currentDate;
+            });
+            this.fechaAVisualizarPorActualidad = upcomingRound.round;
+            console.log(upcomingRound.round);
+        }
+        return this.fechaAVisualizarPorActualidad
+    }
+    
     async showToastMessage(message:string, color: string, icon: string) {
         const toast = await this.toast.create({
             message: message,
@@ -87,6 +231,7 @@ export class ModificarEnfrentamientosPage implements OnInit {
         this.competenciaSeleccionada = $event.name;
         this.idCompetenciaSeleccionada = $event.idcompetition;
         this.anioCompetenciaSeleccionada = $event.anio;
+        this.seleccionoUnaComp = true;
         console.log('Competencia seleccionada: '+this.idCompetenciaSeleccionada + ' con el anio: ' + this.anioCompetenciaSeleccionada);
         this.listEnfrentamientosBD(this.idCompetenciaSeleccionada, this.anioCompetenciaSeleccionada);
     }
@@ -147,5 +292,11 @@ export class ModificarEnfrentamientosPage implements OnInit {
         }).then(alert => {
             alert.present();
         });
+    }
+
+    async fechaEvent($event: any) {
+        console.log($event);
+        const fecha = $event.toString();
+        this.filtrarPartidosPorFecha(fecha);
     }
 }
